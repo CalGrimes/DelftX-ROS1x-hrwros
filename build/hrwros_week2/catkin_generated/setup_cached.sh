bf4d2895bdf6927ec3654c12c98c158f31681c06
@CalGrimes1 <calwgrimes29@gmail.com>
2021-08-22 23:33:47 +0100

    week2 contents

---
+++ build/hrwros_week2/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/cal/hrwros_ws/devel/.private/hrwros_week2:$CMAKE_PREFIX_PATH"
+export PWD="/home/cal/hrwros_ws/build/hrwros_week2"
+export PYTHONPATH="/home/cal/hrwros_ws/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/cal/hrwros_ws/devel/.private/hrwros_week2/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/cal/hrwros_ws/src/hrwros_week2:$ROS_PACKAGE_PATH"
